17f3f0da59aefed67230e926ba79dff5dd6a824f
caimoxuan <391850846@qq.com>
2017-08-18 18:09:52 +0800

    add pageView

--- src/com/cmx/test/entity/PageView.java
+++ src/com/cmx/test/entity/PageView.java
@@ -1,5 +1,91 @@
 package com.cmx.test.entity;
 
-public class PageView {
+import java.io.Serializable;
+import java.util.List;
 
+public class PageView<T> implements Serializable{
+	
+	private Integer pageSize = 10;
+	
+	private Integer pageCount;//总页数
+	
+	private Integer currentPage = 1;//当前页 默认是1
+	
+	private Integer totalRecord;//总记录数
+	
+	private List<T> record;//记录数据
+	
+	private PageIndex pageIndex;//开始和结束页的索引
+	
+	private Integer pageCode;//页码的数量
+	
+	
+	
+	
+	public Integer getPageCode() {
+		return pageCode;
+	}
+
+	public void setPageCode(Integer pageCode) {
+		this.pageCode = pageCode;
+	}
+
+	//获取第一条数据
+	public Integer getFirstResult(){
+		return (this.currentPage - 1) * this.pageSize; 
+	}
+	
+	public void setTotalRecord(Integer totalRecord){
+		this.totalRecord = totalRecord;
+		setPageCount((this.totalRecord + this.pageSize - 1)/this.pageSize);
+		//还有一种算法： this.totalRecord / pageSize == 0?this.totalRecord / pageSize : this.totalRecord / pageSize + 1;
+	}
+
+	public Integer getPageSize() {
+		return pageSize;
+	}
+
+
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+
+	public Integer getPageCount() {
+		return pageCount;
+	}
+
+
+	public void setPageCount(Integer pageCount) {
+		this.pageCount = pageCount;
+		this.pageIndex = PageIndex.getPageIndex(pageCode, currentPage, pageCount);
+	}
+
+
+	public Integer getCurrentPage() {
+		return currentPage;
+	}
+
+
+	public void setCurrentPage(Integer currentPage) {
+		this.currentPage = currentPage;
+	}
+
+
+	public List<T> getRecord() {
+		return record;
+	}
+
+
+	public void setRecord(List<T> record) {
+		this.record = record;
+	}
+
+
+	public Integer getTotalRecord() {
+		return totalRecord;
+	}
+	
+	
+	
 }
